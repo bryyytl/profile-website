6b532247df1d0aa588d6254a1a91573eb5ba0978
Bryan Laporte <bryan.laporte7@gmail.com>
2025-06-27 14:14:13 -0400

    Fixing gap padding and list styling

--- src/app/components/skills/index.tsx
+++ src/app/components/skills/index.tsx
@@ -2,7 +2,7 @@ import { Title } from "@mantine/core";
 import SkillsCards from "./skills-cards";
 
 const Skills = () => (
-  <div className="gap-2 p-4 flex flex-col items-stretch justify-start">
+  <div className="gap-3 p-4 flex flex-col items-stretch justify-start">
     <Title mb="lg" order={2} ta="center">
       What I Do
     </Title>
