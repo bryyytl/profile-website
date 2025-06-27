7a429b4a33f80a3a4ea9607728a5522afd906758
Bryan Laporte <bryan.laporte7@gmail.com>
2025-06-27 13:39:14 -0400

    Fixing dark mode styling in not found and about description

--- src/app/components/about/about-description/index.tsx
+++ src/app/components/about/about-description/index.tsx
@@ -1,8 +1,15 @@
 import { Title } from "@mantine/core";
+import clsx from "clsx";
+import styles from "./aboutdescription.module.css";
 
 const AboutDescription = () => {
   return (
-    <div className="max-w-800 rounded-lg p-4 flex flex-col border-1 border-(--mantine-color-gray-3) bg-[var(--mantine-color-blue-light)] text-[var(--mantine-color-text)] dark:border-(--mantine-color-dark-4)">
+    <div
+      className={clsx(
+        "max-w-800 rounded-lg p-4 flex flex-col bg-[var(--mantine-color-blue-light)] text-[var(--mantine-color-text)]",
+        styles["about-description"],
+      )}
+    >
       <div className="gap-4 flex flex-col items-stretch justify-start">
         <Title order={1} size="h3">
           Hi there, I'm Bryan Laporte!
