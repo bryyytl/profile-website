679a1f4635557cdb860fe71077998d44125f7eb3
Bryan Laporte <bryan.laporte7@gmail.com>
2025-06-28 16:59:57 -0400

    Card mantine title deprecation

--- src/components/personal-interests/personal-interest-cards/personal-interest-card/index.tsx
+++ src/components/personal-interests/personal-interest-cards/personal-interest-card/index.tsx
@@ -1,7 +1,8 @@
-import { Card, Title } from "@mantine/core";
+import CardTitle from "@/components/card-title";
+import type { PersonalInterest } from "@/components/personal-interests/personal-interest-cards/constants";
+import { Card } from "@mantine/core";
 import type { ReactElement } from "react";
 import styles from "./personalinterestcard.module.css";
-import type { PersonalInterest } from "@/components/personal-interests/personal-interest-cards/constants";
 
 const PersonalInterestCard = ({
   description,
@@ -13,9 +14,7 @@ const PersonalInterestCard = ({
   return (
     <Card className={styles.card} padding="xl">
       <Icon size={50} stroke={2} />
-      <Title className={styles["card-title"]} mt="md" order={3} size="h4">
-        {title}
-      </Title>
+      <CardTitle className="mt-4">{title}</CardTitle>
       <p className="text-sm mt-4 text-[var(--mantine-color-dimmed)]">
         {description}
       </p>
--- src/components/skills/skills-cards/skills-card/index.tsx
+++ src/components/skills/skills-cards/skills-card/index.tsx
@@ -1,15 +1,13 @@
-import { Card, List, Title } from "@mantine/core";
-import type { ReactElement } from "react";
-import styles from "./skillscard.module.css";
+import CardTitle from "@/components/card-title";
 import type { Skill } from "@/components/skills/skills-cards/constants";
+import { Card, List } from "@mantine/core";
+import type { ReactElement } from "react";
 
 const SkillsCard = ({ title, listItems }: Skill): ReactElement => {
   return (
     <Card padding="lg">
       <div className="gap-8 flex flex-col items-center justify-start">
-        <Title className={styles["skills-card-title"]} order={3} size="h4">
-          {title}
-        </Title>
+        <CardTitle>{title}</CardTitle>
         <List center pb="md" size="sm" spacing="xs">
           {listItems.map(({ iconElement: Icon, iconDescription }) => (
             <List.Item icon={<Icon fontSize="1.5rem" />} key={iconDescription}>
