cb477cd1565ab2d219554dee9c2424b371415712
Bryan Laporte <bryan.laporte7@gmail.com>
2025-06-30 13:33:01 -0400

    Replacing 404 button mantine component

--- src/components/not-found/return-home-link/index.tsx
+++ src/components/not-found/return-home-link/index.tsx
@@ -1,13 +1,15 @@
-import { Button } from "@mantine/core";
 import Link from "next/link";
 import type { ReactElement } from "react";
 
 const ReturnHomeLink = (): ReactElement => {
   return (
     <div className="flex items-center justify-center">
-      <Button component={Link} fw={500} href="/" size="md">
+      <Link
+        className="rounded-md px-4 py-2 text-md font-medium text-white shadow-xs hover:bg-blue-500 focus-visible:outline-blue-600 bg-[var(--mantine-primary-color-filled)] focus-visible:outline-2 focus-visible:outline-offset-2"
+        href="/"
+      >
         Take me back to home page
-      </Button>
+      </Link>
     </div>
   );
 };
