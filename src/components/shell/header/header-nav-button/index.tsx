e0b947f58d497ea6e9dd14508852df9930f98d96
Bryan Laporte <bryan.laporte7@gmail.com>
2025-06-30 16:01:48 -0400

    Replacing header nav button styling with tailwind css styles

--- src/components/shell/header/header-nav-button/index.tsx
+++ src/components/shell/header/header-nav-button/index.tsx
@@ -2,6 +2,7 @@ import type { NavigationLinkButton } from "@/constants/navigation";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import type { ReactElement } from "react";
+import clsx from "clsx";
 import styles from "./headernavbutton.module.css";
 
 const HeaderNavButton = ({
@@ -11,7 +12,10 @@ const HeaderNavButton = ({
   const pathname = usePathname();
   return (
     <Link
-      className={styles["header-nav-button"]}
+      className={clsx(
+        "rounded-md decoration-none px-3 py-2 text-sm font-medium focus-visible:outline-blue-600 focus-visible:outline-2 focus-visible:outline-offset-2",
+        styles["header-nav-button"],
+      )}
       data-active={pathname === href}
       href={href}
     >
