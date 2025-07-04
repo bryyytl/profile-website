4776a8f813f45a6a776be793f8c94d8cb281ec1f
Bryan Laporte <bryan.laporte7@gmail.com>
2025-07-04 19:22:30 -0400

    Cleaning up scroll styling and no longer necessary mantine theme setup

--- src/components/shell/nav-bar/index.tsx
+++ src/components/shell/nav-bar/index.tsx
@@ -1,25 +1,23 @@
-import { NavLink, ScrollArea } from "@mantine/core";
-import type { ReactElement } from "react";
-import { usePathname } from "next/navigation";
 import { navigationLinkButtons } from "@/constants/navigation";
+import { NavLink } from "@mantine/core";
+import { usePathname } from "next/navigation";
+import type { ReactElement } from "react";
 
 const NavBar = (): ReactElement => {
   const pathname = usePathname();
   return (
-    <ScrollArea>
-      <div className="gap-4 px-1 py-4 flex flex-col items-stretch justify-start">
-        {navigationLinkButtons.map(({ label, href, icon: Icon }) => (
-          <NavLink
-            active={pathname === href}
-            href={href}
-            key={label}
-            label={label}
-            leftSection={<Icon size={16} stroke={1.5} />}
-            variant="subtle"
-          />
-        ))}
-      </div>
-    </ScrollArea>
+    <div className="gap-4 px-1 py-4 flex flex-col items-stretch justify-start overflow-auto">
+      {navigationLinkButtons.map(({ label, href, icon: Icon }) => (
+        <NavLink
+          active={pathname === href}
+          href={href}
+          key={label}
+          label={label}
+          leftSection={<Icon size={16} stroke={1.5} />}
+          variant="subtle"
+        />
+      ))}
+    </div>
   );
 };
 
--- src/components/theme/defaultTheme.ts
+++ src/components/theme/defaultTheme.ts
@@ -1,27 +1,6 @@
-import { ActionIcon, Card, createTheme, ScrollArea } from "@mantine/core";
+import { createTheme } from "@mantine/core";
 
 const defaultTheme = createTheme({
-  components: {
-    ActionIcon: ActionIcon.extend({
-      defaultProps: {
-        radius: "md",
-        size: "lg",
-        variant: "default",
-      },
-    }),
-    Card: Card.extend({
-      defaultProps: {
-        radius: "md",
-        shadow: "sm",
-        withBorder: true,
-      },
-    }),
-    ScrollArea: ScrollArea.extend({
-      defaultProps: {
-        type: "auto",
-      },
-    }),
-  },
   fontFamily: "Roboto, Arial, Verdana, sans-serif",
   headings: {
     fontWeight: "500",
