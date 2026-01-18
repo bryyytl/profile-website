22684241d1513ec723334a4d2aed901f7084fe46
bryyytl <78176335+bryyytl@users.noreply.github.com>
2026-01-18 11:38:08 -0600

    Upgrade next (#9)
    * Initial tools page setup
    
    * Upgraded next version to handle vulnerability

---
+++ src/components/tools/index.tsx
@@ -0,0 +1,14 @@
+import type { ReactElement } from "react";
+
+const Tools = (): ReactElement => {
+  return (
+    <div className="px-4 max-w-3xl my-8 mx-auto">
+      <div className="gap-4 w-inherit flex flex-col items-stretch justify-start">
+        <h1 className="font-medium text-4xl">Tools</h1>
+        <p className="font-medium text-xl">Coming Soon!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Tools;
